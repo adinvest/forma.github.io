c9061219da5c0ac6460f3e1716f202b94ad4408c
adinvest <88491600+adinvest@users.noreply.github.com>
2021-12-11 13:29:52 +0300

    Update process.php

--- php/process.php
+++ php/process.php
@@ -1,8 +1,8 @@
 <?php
 if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) ){
-	$n = $_POST['name'];
-	$e = $_POST['email'];
-	$m = nl2br($_POST['message']);
+    $n = $_POST['name'];
+    $e = $_POST['email'];
+    $m = nl2br($_POST['message']);
     $name = htmlspecialchars($name);
     $email = htmlspecialchars($email);
     $message = htmlspecialchars($message);
@@ -12,14 +12,14 @@ if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) 
     $name = trim($name);
     $email = trim($email);
     $message  = trim($message);
-	$to = "adinvest@list.ru";	
-	$from = $e;
-	$subject = 'Contact Form Message';
-	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
-	$headers = "From: $from\n";
-	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
-	if( mail($to, $subject, $message, $headers) ){
+    $to = "adinvest@list.ru";	
+    $from = $e;
+    $subject = 'Contact Form Message';
+    $message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
+    $headers = "From: $from\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+    if( mail($to, $subject, $message, $headers) ){
 		echo "success";
 	} else {
 		echo "Ошибка. Пожалуйста, попробуйте еще раз.";
